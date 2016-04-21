d99562665a03ccaf7a6fcbea69b8b15be31f389a
Wayne Bishop <bishopwb66@gmail.com>
2016-04-22 09:54:38 +1200

    Added Google map functionality but not correct map

---
+++ js/google-maps.js
@@ -0,0 +1,31 @@
+var tePapaMap;
+
+function initMap() {
+	console.log('test');
+
+	// Get a reference to map container
+	var mapContainer = document.querySelector('#map');
+
+	// Set up some options for the map
+	var mapOptions = {
+		center: {
+			lat: -41.290475,
+			lng: 174.781783
+		},
+		zoom: 15
+	};
+
+	// Create the map
+	tePapaMap = new google.maps.Map(mapContainer, mapOptions);
+
+	// Prepare some markers
+	var tePapaMarker = new google.maps.Marker({
+	    position: {
+			lat: -41.290475,
+			lng: 174.781783
+		},
+	    map: tePapaMap
+	  });
+}
+
+
