0c6eaa8db2283733ee88bf89e773a2f25ab6a6b1
Wayne Bishop <bishopwb66@gmail.com>
2016-04-25 13:20:24 +1200

    Added more validation and patterns for booking page but still a few errors

--- js/patterns.js
+++ js/patterns.js
@@ -1,7 +1,9 @@
 // Patterns to use in validation
 	var usernamePattern = new RegExp("^[a-zA-Z0-9._-]{3,20}$");
 	var namePattern = new RegExp("^[a-zA-Z' .-]{3,50}$");
-	var emailPattern = new RegExp("^[a-zA-Z._@-]{5,20}$");
-	var phonePattern = new RegExp("^[0-9 ]{7,20}$");
-
-
+	var emailPattern = new RegExp("^[a-zA-Z._@-]{5,50}$");
+	var phonePattern = new RegExp("^[0-9 ]{7,25}$");
+	var datePattern = new RegExp("^[0-9 /]{5,25}$")
+	var timePattern = new RegExp("^[a-zA-Z ()]{12,20}$")
+	var passengersPattern = new RegExp("[0-9]{1,2}")
+	var packagePattern = new RegExp("az-A-Z :]{10,50}")
--- js/validation.js
+++ js/validation.js
@@ -8,6 +8,23 @@ var enquiryForm = document.querySelector('#enquiry');
 	var emailMessage = document.querySelector("#email-message");
 	var phoneInput = document.querySelector("#phone");
 	var phoneMessage = document.querySelector("#phone-message");
+	var depDateInput = document.querySelector("#dep-date");
+	var depDateMessage = document.querySelector("#dep-date-message");
+	var depTimeInput = document.querySelector("#dep-time");
+	var depTimeMessage = document.querySelector("#dep-time-message");
+	var passengersInput = document.querySelector("#passengers")
+	var passengersMessage = document.querySelector("#passengers-message")
+	var packageInput = document.querySelector("#package")
+	var packageMessage = document.querySelector("#packageMessage")
+
+
+
+
+
+
+
+
+
 
 	var messageInput = document.querySelector('#comments');
 	var messageMessage = document.querySelector('#message-message')
@@ -19,7 +36,7 @@ nameInput.onblur = function() {
 	if ( this.value.length < 3 ) {
 		nameMessage.innerHTML = 'Name must be at least 3 characters';
 
-	} else if ( this.value.length > 20 ) {
+	} else if ( this.value.length > 50 ) {
 		nameMessage.innerHTML = 'Name must be at most 50 characters';
 
 	} else if ( namePattern.test( this.value ) ) {
@@ -33,13 +50,35 @@ nameInput.onblur = function() {
 
 }
 
+
+emailInput.onblur = function() {
+
+	// Check if meets criteria per pattern
+	if ( this.value.length < 5 ) {
+		emailMessage.innerHTML = 'Email must be at least 5 characters';
+
+	} else if ( this.value.length > 50 ) {
+		emailMessage.innerHTML = 'Email must be at most 50 characters';
+
+	} else if ( emailPattern.test( this.value ) ) {
+
+		emailMessage.innerHTML = '';
+
+	} else {
+
+		emailMessage.innerHTML = 'Invalid email address'
+	}
+
+}
+
+
 phoneInput.onblur = function() {
 
 	// Check if meets criteria per pattern
-	if ( this.value.length < 3 ) {
+	if ( this.value.length < 7 ) {
 		phoneMessage.innerHTML = 'Phone must be at least 7 characters';
 
-	} else if ( this.value.length > 20 ) {
+	} else if ( this.value.length > 25 ) {
 		phoneMessage.innerHTML = 'Phone must be at most 25 characters';
 
 	} else if ( phonePattern.test( this.value ) ) {
@@ -53,25 +92,103 @@ phoneInput.onblur = function() {
 
 }
 
-emailInput.onblur = function() {
+depDateInput.onblur = function() {
 
 	// Check if meets criteria per pattern
-	if ( this.value.length < 3 ) {
-		emailMessage.innerHTML = 'Email must be at least 3 characters';
+	if ( this.value.length < 10 ) {
+		depDateMessage.innerHTML = 'Departure date must be at least 10 characters';
 
-	} else if ( this.value.length > 20 ) {
-		emailMessage.innerHTML = 'Email must be at most 50 characters';
+	} else if ( this.value.length > 15 ) {
+		depDateMessage.innerHTML = 'Departure date must be at most 15 characters';
 
-	} else if ( emailPattern.test( this.value ) ) {
+	} else if ( datePattern.test( this.value ) ) {
 
-		emailMessage.innerHTML = '';
+		depDateMessage.innerHTML = '';
 
 	} else {
 
-		emailMessage.innerHTML = 'Invalid email address'
+		depDateMessage.innerHTML = 'Invalid number. Please use format dd/mm/yyyy';
 	}
 
 }
+
+
+depTimeInput.onblur = function() {
+
+	// Check if meets criteria per pattern
+	if ( this.value.length < 12 ) {
+		depTimeMessage.innerHTML = 'Departure time must be at least 12 characters';
+
+	} else if ( this.value.length > 30 ) {
+		depTimeMessage.innerHTML = 'Departure time must be at most 30 characters';
+
+	} else if ( timePattern.test( this.value ) ) {
+
+		depTimeMessage.innerHTML = '';
+
+	} else {
+
+		depTimeMessage.innerHTML = 'Invalid option. Please use selector';
+	}
+
+}
+
+passengersInput.onblur = function() {
+
+	// Check if meets criteria per pattern
+	if ( this.value.length < 1 ) {
+		passengersMessage.innerHTML = 'Passengers must be at least 1 characters';
+
+	} else if ( this.value.length > 2 ) {
+		passengersMessage.innerHTML = 'Passengers must be at most 2 characters';
+
+	} else if ( passengersPattern.test( this.value ) ) {
+
+		passengersMessage.innerHTML = '';
+
+	} else {
+
+		passengersMessage.innerHTML = 'Invalid number. Please choose insert 1 or 2 digit number only';
+	}
+
+}
+
+
+packageInput.onblur = function() {
+
+	// Check if meets criteria per pattern
+	if ( this.value.length < 10 ) {
+		packageMessage.innerHTML = 'Too few characters. Please choose using the selector';
+
+	} else if ( this.value.length > 50) {
+		packageMessage.innerHTML = 'Too many characters. Please choose using the selector';
+
+	} else if ( packagePattern.test( this.value ) ) {
+
+		packageMessage.innerHTML = '';
+
+	} else {
+
+		packageMessage.innerHTML = 'Invalid option. Please use selector';
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 messageInput.onkeyup = function() {
 
